417395d8a4139ba21391bcbf8bb3bfa888fc764a
Shaanisha <110103376+Shaanisha@users.noreply.github.com>
2024-01-05 19:26:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,22 @@ resource "aws_instance" "myinstance" {
   ami           = "ami-03f4878755434977f"
   instance_type = "t2.micro"
    key_name = "ubuntu_key"
-
+   vpc_security_group_ids = [aws_security_group.instance.id]
   tags = {
     Name = "ubuntu-1"
   }
 }
+
+resource "aws_security_group" "instance" {
+  name      = "instance"
+  description = "allow inbound traffic"
+
+
+ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Open to all sources (for demonstration purposes)
+  }
+}
+
